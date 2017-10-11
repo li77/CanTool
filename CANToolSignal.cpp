5c5f3e154e9b6220191fc5fe913ea01be52c12c6
weixiao <wx_tju@tju.edu.cn>
2017-10-11 21:54:58 +0800

    update

--- CANToolSignal.cpp
+++ CANToolSignal.cpp
@@ -7,16 +7,25 @@ class CANMessage()
 public£º
 	void analysis() 
 	{
-		if (message[0] == 'T') {
-
+		string s_id;
+		string s_dlc;
+		string s_data;
+		if (message[0] == 'T') 
+		{
+			s_id = message.substr(1, 8);
+			s_dlc = message[9];
 		}
-		else if (message[0] == 't') {
-
+		else if (message[0] == 't') 
+		{
+			s_id = message.substr(1, 3);
+			s_dlc = message[4];
 		}
-		else {
+		else 
+		{
 
 		}
 	}
+
 	void setMessage(string _message) 
 	{
 		message = _message;
