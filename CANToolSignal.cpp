516ec6869530d5dd08e86d99b2d60cb5c4e01de2
weixiao <wx_tju@tju.edu.cn>
2017-10-11 19:22:33 +0800

    update

--- CANToolSignal.cpp
+++ CANToolSignal.cpp
@@ -2,23 +2,65 @@
 #include <string>
 using namespace std;
 
-public class CANToolSignal(){
-    
-    private String signal;
-    public void analysis(){
-		if(signal[0] == 'T'){
-			
+class CANMessage()
+{  
+public£º
+	void analysis() 
+	{
+		if (message[0] == 'T') {
+
 		}
-		else if(signal[0] == 't'){
-			
+		else if (message[0] == 't') {
+
 		}
-		else{
-			
+		else {
+
 		}
 	}
-    
-	public void setSignal(String signal){
-		this.signal = signal;
+	void setMessage(string _message) 
+	{
+		message = _message;
 	}
+
+	string getMessage() 
+	{
+		return message;
+	}
+
+	void setId(int _id) 
+	{
+		id = _id;
+	}
+
+	int getId()
+	{
+		return id;
+	}
+
+	void setDlc(int _dlc) 
+	{
+		dlc = _dlc;
+	}
+
+	int getDlc()
+	{
+		return dlc;
+	}
+
+	void setData(int _data) 
+	{
+		data = _data;
+	}
+
+	int getData()
+	{
+		return data;
+	}
+private:
+	string message;
+	int id;
+	int dlc;
+	int data;
+
 
 }
