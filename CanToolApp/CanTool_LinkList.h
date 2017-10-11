861036efbbf8029bfe191186c9ad3f45515e499b
Li Qi <1126632307@qq.com>
2017-10-11 15:05:01 +0800

    update

--- CanToolApp/CanTool_LinkList.h
+++ CanToolApp/CanTool_LinkList.h
@@ -1,11 +1,46 @@
+#ifndef CANTOOL_LINKLIST_H
+#define CANTOOL_LINKLIST_H
 #include <iostream>
+#include <string>
 using namespace std;
 
-typedef struct MessageNode
+typedef unsigned int uint32;
+typedef unsigned char uchar;
+
+struct MessageNode
 {
-	char CANmessage;
-	unint id;
+	char CANmessage = 'BO_';
+	uint32 id;
 	char MessageName;
-	unsigned char DLC;
+	char Separater = ':';
+	uchar DLC;
 	char NodeName;
+	MessageNode* nextMessageNode;
+};
+
+class MessageLinkList
+{
+public:
+	MessageLinkList();
+	~MessageLinkList();
+	void Insert(MessageNode*);
+	void Update(MessageNode*);
+	void Search(MessageNode*);
+	MessageNode* Delete(MessageNode*);
+	void Traversal(MessageNode*);
+
+private:
+	MessageNode* mHead;
+
+};
+
+MessageLinkList::MessageLinkList()
+{
+	mHead = NULL;
 }
+
+MessageLinkList::~MessageLinkList()
+{
+}
+
+#endif
