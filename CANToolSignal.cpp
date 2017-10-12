b8637b5666c30fb3da4a01180d94cf3d36d237ac
weixiao <wx_tju@tju.edu.cn>
2017-10-12 21:05:08 +0800

    string to int

--- CANToolSignal.cpp
+++ CANToolSignal.cpp
@@ -1,10 +1,11 @@
 #include <iostream>
 #include <string>
+#include <stdlib.h>
 using namespace std;
 
-class CANMessage()
+class CANMessage
 {  
-public：
+public:
 	void analysis() 
 	{
 		string s_id;
@@ -13,16 +14,34 @@ public：
 		if (message[0] == 'T') 
 		{
 			s_id = message.substr(1, 8);
+			id = getInt(s_id);
 			s_dlc = message[9];
+			dlc = getInt(s_dlc);
+			if(dlc == 4)
+			{
+				s_data = message.substr(10, 8);
+			}
+			else if (dlc == 8)
+			{
+				s_data = message.substr(10, 16);
+			}
+			data = getInt(s_data);
 		}
 		else if (message[0] == 't') 
 		{
 			s_id = message.substr(1, 3);
+			id = getInt(s_id);
 			s_dlc = message[4];
-		}
-		else 
-		{
-
+			dlc = getInt(s_dlc);
+			if (dlc == 4)
+			{
+				s_data = message.substr(5, 8);
+			}
+			else if (dlc == 8)
+			{
+				s_data = message.substr(5, 16);
+			}
+			data = getInt(s_data);
 		}
 	}
 
@@ -65,11 +84,37 @@ public：
 	{
 		return data;
 	}
+
 private:
 	string message;
 	int id;
 	int dlc;
 	int data;
 
+	//字符串中16进制数转10进制int
+	int getInt(string _str) 
+	{
+		const char *p = _str.c_str();
+		char *str;
+		int num = (int)strtol(p, &str, 16);
+		return num;
+	}
 
+
+};
+
+int main()
+{
+	CANMessage canMessage;
+	canMessage.setMessage("T1234567F81122334455667788\r");
+	canMessage.analysis();
+	int id = canMessage.getId();
+	int dlc = canMessage.getDlc();
+	int data = canMessage.getData();
+	cout << id << endl;
+	cout << dlc << endl;
+	cout << data << endl;
+	cin >> id;
+
+	return 0;
 }
