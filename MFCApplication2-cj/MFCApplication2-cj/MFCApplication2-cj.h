6207efb947164d93e9d2a50b3cd3c0ef28ff0933
swear291 <937589191@qq.com>
2017-10-17 20:38:49 +0800

    cj

---
+++ MFCApplication2-cj/MFCApplication2-cj/MFCApplication2-cj.h
@@ -0,0 +1,32 @@
+
+// MFCApplication2-cj.h : PROJECT_NAME 应用程序的主头文件
+//
+
+#pragma once
+
+#ifndef __AFXWIN_H__
+	#error "在包含此文件之前包含“stdafx.h”以生成 PCH 文件"
+#endif
+
+#include "resource.h"		// 主符号
+
+
+// CMFCApplication2cjApp: 
+// 有关此类的实现，请参阅 MFCApplication2-cj.cpp
+//
+
+class CMFCApplication2cjApp : public CWinApp
+{
+public:
+	CMFCApplication2cjApp();
+
+// 重写
+public:
+	virtual BOOL InitInstance();
+
+// 实现
+
+	DECLARE_MESSAGE_MAP()
+};
+
+extern CMFCApplication2cjApp theApp;
